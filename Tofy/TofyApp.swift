074bd0a628e2fb3ce10dab32802b8e3b397e4f35
Mikel Lopez <mikel_lopez_salazar@carrefour.com>
2020-12-02 17:22:59 +0100

    Initial Commit

---
+++ Tofy/TofyApp.swift
@@ -0,0 +1,20 @@
+//
+//  TofyApp.swift
+//  Tofy
+//
+//  Created by usuario on 02/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TofyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
